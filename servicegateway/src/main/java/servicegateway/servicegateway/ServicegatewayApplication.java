a89281d8dcdc3e2ff8681b4496ff8dd76651eb8d
vikramgundkar1 <110653893+vikramgundkar1@users.noreply.github.com>
2024-09-23 20:17:26 +0530

    added gateway service

---
+++ servicegateway/src/main/java/servicegateway/servicegateway/ServicegatewayApplication.java
@@ -0,0 +1,13 @@
+package servicegateway.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicegatewayApplication.class, args);
+	}
+
+}
---
+++ servicegateway/src/test/java/servicegateway/servicegateway/ServicegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package servicegateway.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicegatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
